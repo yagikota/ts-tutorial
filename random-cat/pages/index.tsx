f115534c274083c818c9a0507e6f31ba7b65d972
Kota <mdryzk64smsh@gmail.com>
2023-04-15 21:16:47 +0900

    ボタンをクリックしたときに画像が更新されるようにする

--- random-cat/pages/index.tsx
+++ random-cat/pages/index.tsx
@@ -13,8 +13,19 @@ const IndexPage: NextPage = () => {
             setLoading(false); // ローディング状態を更新する
         });
     }, []); // 第2引数はどのタイミングで処理内容を実行するかの指定
-    // ❸ ローディング中でなければ、画像を表示する
-    return <div>{loading || <img src={imageUrl} />}</div>;
+
+    const handleClick = async () => {
+        setLoading(true); // 読込中フラグを立てる
+        const newImage = await fetchImage();
+        setImageUrl(newImage.url); // 画像URLの状態を更新する
+        setLoading(false); // 読込中フラグを倒す
+    };
+    return (
+        <div>
+            <button onClick={handleClick}>他のにゃんこも見る</button>
+            <div>{loading || <img src={imageUrl} />}</div>
+        </div>
+    );
 };
 export default IndexPage;
 
@@ -24,9 +35,26 @@ type Image = {
 // MEMO: async/await: 非同期処理をより同期処理と同じような文脈で書くことができる
 const fetchImage = async (): Promise<Image> => {
     const res = await fetch("https://api.thecatapi.com/v1/images/search");
-    const images = await res.json();
-    console.log(images);
-    return images[0];
+    const images: unknown = await res.json();
+    if (!Array.isArray(images)) {
+        throw new Error("猫の画像が取得できませんでした");
+    }
+    const image: unknown = images[0];
+    // Imageの構造をなしているか？
+    if (!isImage(image)) {
+        throw new Error("猫の画像が取得できませんでした");
+    }
+    return image;
+};
+
+// TODO: 型ガード関数について調べる
+const isImage = (value: unknown): value is Image => {
+    // 値がオブジェクトなのか？
+    if (!value || typeof value !== "object") {
+        return false;
+    }
+    // urlプロパティが存在し、かつ、それが文字列なのか？
+    return "url" in value && typeof value.url === "string";
 };
 
 fetchImage().then((image) => {
